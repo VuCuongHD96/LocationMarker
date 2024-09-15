b3dd5791a55fe7377d0e3cf0b4b32f523bf2bafb
VuCuongHD96 <xuancuonghd1102@gmail.com>
2024-09-15 13:35:46 +0700

    Initial Commit

---
+++ LocationMarker/LocationMarkerApp.swift
@@ -0,0 +1,17 @@
+//
+//  LocationMarkerApp.swift
+//  LocationMarker
+//
+//  Created by Work on 15/9/24.
+//
+
+import SwiftUI
+
+@main
+struct LocationMarkerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
