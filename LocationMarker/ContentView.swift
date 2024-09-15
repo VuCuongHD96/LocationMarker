17ea784cebe1da6609b64357baf18954cddb1bab
VuCuongHD96 <xuancuonghd1102@gmail.com>
2024-09-15 13:38:46 +0700

    Location select marker

--- LocationMarker/ContentView.swift
+++ LocationMarker/ContentView.swift
@@ -6,19 +6,44 @@
 //
 
 import SwiftUI
+import MapKit
 
 struct ContentView: View {
+    
+    let array: [Location] = [
+        .init(coor: .weequahicPark),
+        .init(coor: .empireStateBuilding),
+        .init(coor: .columbiaUniversity)
+    ]
+    
+    @State private var selectedTag: String?
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        Map(selection: $selectedTag) {
+            ForEach(array) { item in
+                Marker("Empire state building", coordinate: item.coor)
+                    .tint(.orange)
+                    .tag(item.id)
+            }
+  
         }
-        .padding()
+        .onChange(of: selectedTag) { oldValue, newValue in
+            print("--- debug --- newValue = ", newValue)
+        }
     }
 }
 
+extension CLLocationCoordinate2D {
+    static let weequahicPark = CLLocationCoordinate2D(latitude: 40.7063, longitude: -74.1973)
+    static let empireStateBuilding = CLLocationCoordinate2D(latitude: 40.7484, longitude: -73.9857)
+    static let columbiaUniversity = CLLocationCoordinate2D(latitude: 40.8075, longitude: -73.9626)
+}
+
+
 #Preview {
     ContentView()
+}
+struct Location: Identifiable {
+    let id = UUID().uuidString
+    let coor: CLLocationCoordinate2D
 }
